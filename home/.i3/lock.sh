09e3d4d1db2a4d4a98a3f92b7274a05699e8a4e2
Jerko Steiner <jerko.steiner@gmail.com>
2017-09-24 00:55:34 -0400

    home/.i3/lock.sh: Remember previously set dpms value

--- home/.i3/lock.sh
+++ home/.i3/lock.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+# remember currently set dpms
+dpms=$(xset q dpms | grep -E '(Standby|Suspend|Off): [0-9]+' | sed 's/[a-zA-Z: ]\+/ /g')
+
 # set screen timeout
 xset dpms 5 5 5
 
 i3lock --nofork -c 004477
 
 # disable screen timeout after unlocking
-xset dpms 0 0 0
+xset dpms ${dpms}
