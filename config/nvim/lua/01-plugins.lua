add52a7483d98ea5c071df360bc4a5cd8a544673
Jerko Steiner <jerko.steiner@gmail.com>
2023-05-05 12:33:54 +0200

    nvim: Configure LSP for lua

--- config/nvim/lua/01-plugins.lua
+++ config/nvim/lua/01-plugins.lua
@@ -8,6 +8,29 @@ lspconfig.rust_analyzer.setup {
   },
 }
 
+lspconfig.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
 -- Ensure floating preview window has max width.
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
 function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
